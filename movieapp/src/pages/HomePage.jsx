9102a76a8fe9c1959b96d4a97c552595c6d1101f
amer <amerissa@outlook.com>
2023-11-22 05:30:09 -0500

    pair programming, UI and moviepage backend

--- movieapp/src/pages/HomePage.jsx
+++ movieapp/src/pages/HomePage.jsx
@@ -1,3 +1,4 @@
+
 import React, {useState, useEffect} from 'react';
 import '../App.css';
 import { Container } from 'react-bootstrap';
@@ -6,6 +7,8 @@ import ExampleImage from '../Assets/movieexample.jpg';
 import { FiArrowRight } from "react-icons/fi";
 import BannerBackground from '../Assets/blackgreygradient.jpg';
 // import Navbar from './Navbar';
+import { BiSolidRightArrow } from 'react-icons/bi';
+
 
 import MovieQueue from '../util/MovieQueue';
 import { Link } from 'react-router-dom';
@@ -20,7 +23,7 @@ const options = {
   method: 'GET',
   headers: {
     accept: 'application/json',
-    Authorization: '' + auth
+    Authorization: auth + ''
   }
 };
 
@@ -102,7 +105,35 @@ const Home = () => {
             Start Here <FiArrowRight />
           </button>
         </center>
-        
+        <div className="UpcomingMovies-section-container">
+          <div className="UpcomingMovies-text-section-container">
+              <center>
+              Upcoming Movies <BiSolidRightArrow />
+              </center>
+              </div>
+              <div className="UpcomingMovies-images">
+                  <Link to={`/movie/${queue.get(0) == null ? '' : queue.get(0).page_title}`}>
+                      <img className='MovieImage-size' src={queue.get(0) == null ? '' : queue.get(0).poster} alt=''></img>
+                  </Link>
+
+                  <Link to={`/movie/${queue.get(1) == null ? '' : queue.get(1).page_title}`}>
+                      <img className='MovieImage-size'src={queue.get(1) == null ? '' : queue.get(1).poster} alt=''></img>
+                  </Link>
+
+                  <Link to={`/movie/${queue.get(2) == null ? '' : queue.get(2).page_title}`}>
+                      <img className='MovieImage-size'src={queue.get(2) == null ? '' : queue.get(2).poster} alt=''></img>
+                  </Link>
+
+                  <Link to={`/movie/${queue.get(3) == null ? '' : queue.get(3).page_title}`}>
+                      <img className='MovieImage-size'src={queue.get(3) == null ? '' : queue.get(3).poster} alt=''></img>
+                  </Link>
+
+                  <Link to={`/movie/${queue.get(4) == null ? '' : queue.get(4).page_title}`}>
+                      <img className='MovieImage-size'src={queue.get(4) == null ? '' : queue.get(4).poster} alt=''></img>
+                  </Link>
+
+          </div>
+      </div>    
         {/* <center>
           <button className="secondary-button">
             Start Here <FiArrowRight />
@@ -112,5 +143,92 @@ const Home = () => {
     </div>
   )
 };
+
+const UpcomingMovies = () => {
+  const queue = new MovieQueue();
+  const results = [];
+
+  const [firstResults, setFirstResults] = useState([])
+  const [secondResults, setSecondResults] = useState([])
+  const [thirdResults, setThirdResults] = useState([])
+
+  useEffect(() => {
+      fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&primary_release_date.gte=' + todaysDate[2] + '-' + todaysDate[0] + '-' + todaysDate[1] + '&sort_by=popularity.desc', options)
+          .then(res => res.json())
+          .then(json => setFirstResults(json.results))
+  }, []);
+
+  useEffect(() => {
+      fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=2&primary_release_date.gte=' + todaysDate[2] + '-' + todaysDate[0] + '-' + todaysDate[1] + '&sort_by=popularity.desc', options)
+          .then(res => res.json())
+          .then(json => setSecondResults(json.results))
+  }, []);
+
+  useEffect(() => {
+      fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=4&primary_release_date.gte=' + todaysDate[2] + '-' + todaysDate[0] + '-' + todaysDate[1] + '&sort_by=popularity.desc', options)
+          .then(res => res.json())
+          .then(json => setThirdResults(json.results))
+  }, []);
+
+  // The rest
+
+  firstResults.forEach(res => results.push(res));
+  secondResults.forEach(res => results.push(res));
+  thirdResults.forEach(res => results.push(res));
+
+  for (let i = 0; i < results.length; i++) {
+      const result = results[i];
+      const releaseDate = result.release_date.split('-');
+
+      if ((parseInt(releaseDate[0]) === 2024 && parseInt(releaseDate[1]) > todaysDate[0] - 6) || parseInt(releaseDate[0]) > 2024) {
+          continue;
+      }
+      const lowercase = result.original_title.replaceAll(' ', '-').toLowerCase() + '-' + releaseDate[0];
+
+      const map = {
+          title: result.original_title,
+          page_title: lowercase,
+          description: result.overview,
+          release: result.release_date,
+          poster: 'https://image.tmdb.org/t/p/original' + result.poster_path,
+          backdrop: result.backdrop_path,
+          popularity: result.popularity
+      };
+      queue.enqueue(map);
+  }
+  // queue.sort();
+
+  // return (
+  //     <div className="UpcomingMovies-section-container">
+  //         <div className="UpcomingMovies-text-section-container">
+  //             <center>
+  //             Upcoming Movies <BiSolidRightArrow />
+  //             </center>
+  //             </div>
+  //             <div className="UpcomingMovies-images">
+  //                 <Link to={`/movie/${queue.get(0) == null ? '' : queue.get(0).page_title}`}>
+  //                     <img className='MovieImage-size' src={queue.get(0) == null ? '' : queue.get(0).poster} alt=''></img>
+  //                 </Link>
+
+  //                 <Link to={`/movie/${queue.get(1) == null ? '' : queue.get(1).page_title}`}>
+  //                     <img className='MovieImage-size'src={queue.get(1) == null ? '' : queue.get(1).poster} alt=''></img>
+  //                 </Link>
+
+  //                 <Link to={`/movie/${queue.get(2) == null ? '' : queue.get(2).page_title}`}>
+  //                     <img className='MovieImage-size'src={queue.get(2) == null ? '' : queue.get(2).poster} alt=''></img>
+  //                 </Link>
+
+  //                 <Link to={`/movie/${queue.get(3) == null ? '' : queue.get(3).page_title}`}>
+  //                     <img className='MovieImage-size'src={queue.get(3) == null ? '' : queue.get(3).poster} alt=''></img>
+  //                 </Link>
+
+  //                 <Link to={`/movie/${queue.get(4) == null ? '' : queue.get(4).page_title}`}>
+  //                     <img className='MovieImage-size'src={queue.get(4) == null ? '' : queue.get(4).poster} alt=''></img>
+  //                 </Link>
+
+  //         </div>
+  //     </div>    
+  // );
+}
 
 export default Home;
